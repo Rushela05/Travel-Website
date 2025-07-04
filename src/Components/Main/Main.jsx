5ba4c1ed7066b26ba86349b0c45898bf3bdbe3ec
Rushela05 <rushelanp05@gmail.com>
2025-07-05 00:32:10 +0530

    deleting the videos and imagws to add new ones

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,6 +1,18 @@
 import React from "react";
 import './main.css'
 
+const Data = [
+    {
+        id:1,
+        imgSrc: img,
+        destTitle: 'Bora Bora',
+        location: 'New Zealand',
+        grade: 'CULTURAL RELAX',
+        fees: '$700',
+        description: 'The epitome of romance, Bora Bora is one of the best travel destinations in the world. This place is known for its luxurious stays and adventurous activities.'
+    },
+]
+
 const Main = () => {
     return(
         <section className="main container section">
