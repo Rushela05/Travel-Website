34dc18fb9a8ddca699e4f391e8de51bcf616e000
Rushela05 <rushelanp05@gmail.com>
2025-07-04 14:07:43 +0530

    adding the form to add the details

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import './home.scss'
 import video from '../../Assets/video 1.mp4'
+import { GrLocation } from "react-icons/gr";
 
 const Home = () => {
     return(
@@ -18,7 +19,34 @@ const Home = () => {
                     <h1 className="homeTitle">
                         Search your holiday
                     </h1>
-                    
+
+                </div>
+
+                <div className="cardDiv grid">
+                    <div className="destinationInput">
+                        <label htmlFor="city">Search your destination</label>
+                        <div className="input flex">
+                            <input type="text" placeholder="Enter name here..." />
+                            <GrLocation className="icon"/>
+                        </div>
+                    </div>
+
+                    <div className="dateInput">
+                        <label htmlFor="city">Select your date:</label>
+                        <div className="input flex">
+                            <input type="date"/>
+                        </div>
+                    </div>
+
+                    <div className="priceInput">
+                        <div className="label_total flex">
+                            <label htmlFor="price">Max price:</label>
+                            <h3 className="total">$5000</h3>
+                        </div>
+                        <div className="input flex">
+                            <input type="range" max="5000" min="1000" />
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
