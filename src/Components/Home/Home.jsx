dd34e152b2d0eb2922ff3711bdd74f6541a9f488
Rushela05 <rushelanp05@gmail.com>
2025-07-04 13:47:13 +0530

    Adding the basic content to the home page

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -7,6 +7,20 @@ const Home = () => {
         <section className="home">
             <div className="overlay"></div>
             <video src={video} muted autoPlay loop type of="video 1.mp4"></video>
+
+            <div className="homeContent container">
+                <div className="textDiv">
+
+                    <span className="smallText">
+                        Our Packages
+                    </span>
+
+                    <h1 className="homeTitle">
+                        Search your holiday
+                    </h1>
+                    
+                </div>
+            </div>
         </section>
     )
     
