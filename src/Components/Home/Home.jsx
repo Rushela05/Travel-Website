f34b7ffa2f7c08b5225031e8682fca8dec3a2f06
Rushela05 <rushelanp05@gmail.com>
2025-07-04 13:41:28 +0530

    adding a video to the top of the homepage

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import './home.css'
+import video from '../../Assets/video 1.mp4'
 
 const Home = () => {
     return(
         <section className="home">
             <div className="overlay"></div>
-            <video src=""></video>
+            <video src={video} muted autoPlay loop type of="video 1/mp4"></video>
         </section>
     )
     
