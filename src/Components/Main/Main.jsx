a07a46e01046e67bdeed4bbcd6e8e9723fa44f2b
Rushela05 <rushelanp05@gmail.com>
2025-07-05 00:57:29 +0530

    adding images and creating the main page

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,5 +1,8 @@
 import React from "react";
-import './main.css'
+import './main.scss'
+import img from '../../Assets/image1.jpg'
+import { HiOutlineLocationMarker } from "react-icons/hi";
+import { LuClipboardCheck } from "react-icons/lu";
 
 const Data = [
     {
@@ -23,7 +26,42 @@ const Main = () => {
             </div>
 
             <div className="secContent grid">
-                
+                {
+                    Data.map(({id, imgSrc, destTitle, location, grade, fees, description})=>{
+                        return(
+                            <div key={id} className="singleDestination">
+                                <div className="imageDiv">
+                                    <img src={imgSrc} alt={destTitle} />
+                                </div>
+
+                                <div className="cardInfo">
+                                    <h4 className="destTitle flex">{destTitle}</h4>
+                                    <span className="continent flex">
+                                        <HiOutlineLocationMarker className="icon" />
+                                        <span className="name">{location}</span>
+                                    </span>
+
+                                    <div className="fees flex">
+                                        <div className="grade flex">
+                                            <span>{grade}<small>+1</small></span>
+                                        </div>
+                                        <div className="price flex">
+                                            <h5>{fees}</h5>
+                                        </div>
+                                    </div>
+
+                                    <div className="desc flex">
+                                        <p>{description}</p>
+                                    </div>
+
+                                    <button className="btn flex">
+                                        Details <LuClipboardCheck className="icon" />
+                                    </button>
+                                </div>
+                            </div>
+                        )
+                    }
+                )}
             </div>
         </section>
     )
